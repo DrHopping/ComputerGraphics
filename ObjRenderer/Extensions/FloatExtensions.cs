2d4f60637ad67c6a287e5199a23d5fb6433f87bd
Mykyta Pedorenko <mykyta_pedorenko@epam.com>
2021-04-12 21:44:42 +0300

    Added EqualsEpsilon float extension method

---
+++ ObjRenderer/Extensions/FloatExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ObjRenderer.Extensions
+{
+    public static class FloatExtensions
+    {
+        public const float Epsilon = 0.0001f;
+
+        public static bool EqualsEpsilon(this float a, float b)
+        {
+            return Math.Abs(a - b) < Epsilon;
+        }
+
+    }
+}
