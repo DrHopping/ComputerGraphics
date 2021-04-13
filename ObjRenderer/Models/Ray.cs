40857c34eb29759f170a89b6baaf2594b4eeeec3
Mykyta Pedorenko <mykyta_pedorenko@epam.com>
2021-04-13 23:08:34 +0300

    Added Ray clas

---
+++ ObjRenderer/Models/Ray.cs
@@ -0,0 +1,30 @@
+﻿using ObjRenderer.Matrices;
+using ObjRenderer.Tuples;
+
+namespace ObjRenderer.Models
+{
+    public class Ray
+    {
+        public Point Origin { get; }
+        public Vector Direction { get; }
+
+        public Ray(Point origin, Vector direction)
+        {
+            Origin = origin;
+            Direction = direction;
+        }
+
+        public Point Position(float t)
+        {
+            return Origin + Direction * t;
+        }
+
+        public Ray Transform(Matrix transform)
+        {
+            var origin = (Point)(transform * Origin);
+            var direction = (Vector)(transform * Direction);
+
+            return new Ray(origin, direction);
+        }
+    }
+}
