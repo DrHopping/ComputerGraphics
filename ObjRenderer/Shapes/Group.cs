202501a4a6f65a93c235d878695889ec94e5bdce
Mykyta Pedorenko <mykyta_pedorenko@epam.com>
2021-04-13 23:09:01 +0300

    Added Group shape class

---
+++ ObjRenderer/Shapes/Group.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ObjRenderer.Intersections;
+using ObjRenderer.Models;
+using ObjRenderer.Tuples;
+
+namespace ObjRenderer.Shapes
+{
+    public class Group : Shape, IEnumerable<Shape>
+    {
+        private List<Shape> _shapes;
+
+        public Group(IEnumerable<Shape> shapes) : this()
+        {
+            AddChildren(shapes);
+        }
+
+        public Group()
+        {
+            _shapes = new List<Shape>();
+        }
+
+        public void AddChild(Shape shape)
+        {
+            _shapes.Add(shape);
+        }
+
+        public void AddChildren(IEnumerable<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                AddChild(shape);
+            }
+        }
+
+        public override IntersectionCollection LocalIntersect(Ray ray)
+        {
+            var intersects = _shapes.SelectMany(shape => shape.Intersect(ray)).ToArray();
+
+            return new IntersectionCollection(intersects);
+        }
+
+        public override Vector LocalNormalAt(Point point, IntersectionWithUV hit = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shape this[int i] => _shapes[i];
+
+        public IEnumerator<Shape> GetEnumerator()
+        {
+            return _shapes.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
